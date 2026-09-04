2bd42bc2da8bad8e9b2a6dcc1f661d68ab093667
Code-Amare <amisgana809@gmail.com>
2026-09-04 16:47:14 +0300

    feat: setup django with api app

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class TestView(APIView):
+    def get(self, request):
+        return Response(
+            {"detail": "Backend works!"},
+            status=status.HTTP_200_OK,
+        )
